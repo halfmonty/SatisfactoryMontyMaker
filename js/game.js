e8a9da61b0cbe16eab17332eeb58fcd99963e00c
halfmonty <mikehubley@gmail.com>
2019-03-08 22:19:54 -0600

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -28,7 +28,7 @@ function preload() {
     this.load.spritesheet("mario", "./assets/mario.png", { frameWidth: 40, frameHeight: 40 });
     this.load.atlas('player', './assets/marioSprites.png', './assets/mario.json');
 
-    this.time.advancedTiming = true;
+    //this.time.advancedTiming = true;
 }
 
 function create() {
@@ -40,13 +40,34 @@ function create() {
     this.physics.world.bounds.width = groundLayer.width;
     this.physics.world.bounds.height = groundLayer.height;
 
-    player = this.physics.add.sprite(200, 200, 'player'); 
-    //player.setBounce(0.2); // our player will bounce from items
-    player.setCollideWorldBounds(true); // don't go out of the map
-    //player.body.setAllowDrag(true);
-    //player.body.setDrag(700, 0);
-    //player.body.setFriction(1, 1);
+    player = this.physics.add.sprite(200, 200, 'player', 'mario_05');
+    player.setCollideWorldBounds(true);
     this.physics.add.collider(groundLayer, player);
+
+    this.anims.create({
+        key: 'runRight',
+        frames: [{ key: 'player', frame: 'mario_05'}, { key: 'player', frame: 'mario_08'}],
+        frameRate: 15,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'runLeft',
+        frames: [{ key: 'player', frame: 'mario_04'}, { key: 'player', frame: 'mario_01'}],
+        frameRate: 15,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'runFastRight',
+        frames: [{ key: 'player', frame: 'mario_081'}, { key: 'player', frame: 'mario_082'}],
+        frameRate: 15,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'runFastLeft',
+        frames: [{ key: 'player', frame: 'mario_083'}, { key: 'player', frame: 'mario-084'}],
+        frameRate: 15,
+        repeat: -1
+    });
 
     cursors = this.input.keyboard.createCursorKeys();
 
@@ -73,34 +94,81 @@ function create() {
 
     this.jumped = false;
     this.momentum = 0;
+    this.direction = 1;
 }
 
 function update() {
+    if(player.body.velocity.y > 0  && !player.body.onFloor()){
+        if(this.direction == 0){
+            player.setTexture('player', 'mario_12');
+        }else {
+            player.setTexture('player', 'mario_15');
+        }
+    }else if(player.body.velocity.y < 0  && !player.body.onFloor()){
+        if(this.direction == 0){
+            player.setTexture('player', 'mario_13');
+        }else {
+            player.setTexture('player', 'mario_14');
+        }
+        if(player.body.velocity.y >= 400){
+            player.body.velocity.y = 400;
+        }
+    }
+
+    if(player.body.velocity.x == 0 && player.body.velocity.y == 0 && player.body.onFloor()){
+        if(this.direction == 0){
+            player.setTexture('player', 'mario_04');
+        }else {
+            player.setTexture('player', 'mario_05');
+        }
+    }
 
     if (cursors.left.isDown) // if the left arrow key is down
     {
+        this.direction = 0;
+        
         if(player.body.onFloor()){
             this.momentum++;
         }
         if(this.momentum < 15){
             player.body.velocity.x = -this.slowRun;
+            if(player.body.onFloor()){
+                player.anims.play('runLeft', true);
+            }
         }else if(this.momentum < 50){
             player.body.velocity.x = -this.mediumRun;
+            if(player.body.onFloor()){
+                player.anims.play('runLeft', true);
+            }
         }else{
+            if(player.body.onFloor()){
+                player.anims.play('runFastLeft', true);
+            }
             player.body.velocity.x = -this.fastRun;
         }
         
     }
     else if (cursors.right.isDown) // if the right arrow key is down
     {
+        this.direction = 1;
+        
         if(player.body.onFloor()){
             this.momentum++;
         }
         if(this.momentum < 15){
             player.body.velocity.x = this.slowRun;
+            if(player.body.onFloor()){
+                player.anims.play('runRight', true);
+            }
         }else if(this.momentum < 50){
             player.body.velocity.x = this.mediumRun;
+            if(player.body.onFloor()){
+                player.anims.play('runRight', true);
+            }
         }else{
+            if(player.body.onFloor()){
+                player.anims.play('runFastRight', true);
+            }
             player.body.velocity.x = this.fastRun;
         }
     } else if (!cursors.left.isDown && !cursors.right.isDown) {
@@ -121,13 +189,13 @@ function update() {
             this.jumped = true;
         }
     }else if((cursors.space.isUp && cursors.up.isUp)){
-        if(this.jumped){
-            player.body.setVelocityY(0);
+        if(this.jumped && player.body.velocity.y < 0){
+            player.body.setVelocityY(-60);
         }
         this.jumped = false;
     }
 }
 
 function render(){
-    this.debug.text('FPS: ' + this.time.fps || 'FPS: --', 40, 40, "#00ff00");
+    //this.debug.text('FPS: ' + this.time.fps || 'FPS: --', 40, 40, "#00ff00");
 }
